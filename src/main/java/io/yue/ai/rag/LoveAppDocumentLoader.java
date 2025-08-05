2dce7507eda7430afcfbd58b7b6779108dc8e8ed
siehun <126447514+siehun@users.noreply.github.com>
2025-08-06 00:16:35 +0800

    文档加载

---
+++ src/main/java/io/yue/ai/rag/LoveAppDocumentLoader.java
@@ -0,0 +1,42 @@
+package io.yue.ai.rag;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class LoveAppDocumentLoader {
+    private final ResourcePatternResolver resourcePatternResolver;
+    LoveAppDocumentLoader(ResourcePatternResolver resourcePatternResolver) {
+        this.resourcePatternResolver = resourcePatternResolver;
+    }
+    public List<Document> loadMarkdowns() {
+        List<Document> allDocuments = new ArrayList<>();
+        try {
+            Resource[] resources = resourcePatternResolver.getResources("classpath:document/*.md");
+            for (Resource resource : resources) {
+                String filename = resource.getFilename();
+                MarkdownDocumentReaderConfig config= MarkdownDocumentReaderConfig.builder()
+                        .withHorizontalRuleCreateDocument(true)
+                        .withIncludeBlockquote(false)
+                        .withIncludeCodeBlock(false)
+                        .withAdditionalMetadata("filename", filename)
+                        .build();
+                MarkdownDocumentReader reader = new MarkdownDocumentReader(resource, config);
+                allDocuments.addAll(reader.get());
+            }
+        } catch (IOException e) {
+            log.error("markdown文件加载失败", e);
+        }
+        return allDocuments;
+    }
+}
